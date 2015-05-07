48bfbeda747296c7953334d6aae04643d9f2d7ea
Annie Daniel <adaniel@texastribune.org>
2015-05-07 13:31:26 -0500

    teacher and student fields for campus

--- scuole/campus/models.py
+++ scuole/campus/models.py
@@ -21,31 +21,95 @@ class Campus(models.Model):
     latitude = models.FloatField(help_text="Campus latitude")
     longitude = models.FloatField(help_text="Campus longitude")
     # TAPR fields
+    # C_RATING
     rating = models.CharField(
         help_text="Campus rating", max_length=1)
+    # CAMPUS
     campus_id = models.CharField(
         help_text="Campus ID", max_length=10)
+    # CFLCHART
     charter = models.CharField(
         help_text="Chartered campus", max_length=1)
+    # COUNTY
     county = models.CharField(
-        help_text="County campus is located", max_length=100)
+        help_text="County number campus is located", max_length=100)
+    # DISTRICT
     district = models.CharField(
         help_text="District number campus is located", max_length=100)
+    # GRDSPAN
     grade_span = models.CharField(
         help_text="Campus span of grade levels based on enrollment",
         max_length=6)
+    # GRDTYPE
     level = models.CharField(
         help_text="Campus type: Elementary, Middle, Senior or Both",
         max_length=1)
-    # student breakdown
+    # student breakdown 2014
+    # CPETALLC
     all_students = models.IntegerField(
         help_text="Campus total student enrollment")
+    # CPETBLAP
     african_american = models.FloatField(
         help_text="Percent of African American students enrolled at campus")
+    # CPETHISP
     hispanic = models.FloatField(
         help_text="Percent of Hispanic students enrolled at campus")
+    # CPETASIP
     asian = models.FloatField(
         help_text="Percent of Asian students enrolled at campus")
+    # CPETINDP
+    native_american = models.FloatField(
+        help_text="Percent of Native American students enrolled at campus")
+    # CPETTWOP
+    two_or_more = models.FloatField(
+        help_text="Percent of students of two-or-more races enrolled at campus")
+    # CPETWHIP
+    white = models.FloatField(
+        help_text="Percent of white students enrolled at campus")
+    # CPETRSKP
+    at_risk = models.FloatField(
+        help_text="Percent of at-risk students enrolled at campus")
+    # CPETBILP
+    esl = models.FloatField(
+        help_text="Percent of ESL students enrolled at campus")
+    # CPETECOP
+    econ_disadv = models.FloatField(
+        help_text="Percent of economocally disadvantaged students enrolled at campus")
+    # CPETSPEP
+    special_ed = models.FloatField(
+        help_text="Percent of special education students enrolled at campus")
+    # Teacher and staff breakdown 2014
+    # CPSATOFC
+    total_staff = models.FloatField(
+        help_text="Total full time staff at campus")
+    # CPSSTOSA
+    admin_salary = models.FloatField(
+        help_text="School admin total base salary average at campus")
+    # CPSTTOSA
+    teacher_salary = models.FloatField(
+        help_text="Teacher full time base salary average at campus")
+    # CPST00SA
+    beginning_salary = models.FloatField(
+        help_text="Teacher beginning base salary average at campus")
+    # CPST01SA
+    teacher_1_5y_salary = models.FloatField(
+        help_text="Teacher 1-5 years experience base salary average at campus")
+    # CPST06SA
+    teacher_6_10y_salary = models.FloatField(
+        help_text="Teacher 6-10 years experience base salary average at campus")
+    # CPST11SA
+    teacher_11_20y_salary = models.FloatField(
+        help_text="Teacher 11-20 years experience base salary average at campus")
+    # CPST20SA
+    teacher_20y_plus_salary = models.FloatField(
+        help_text="Teacher more than 20 years experience base salary average")
+    # s
+    # CPSTEXPA
+    teacher_experience = models.FloatField(
+        help_text="Average teacher experience at campus")
+    #  CPSTKIDR
+    teacher_student_ratio = models.FloatField(
+        help_text="Teacher student ratio at campus")
 
     def __unicode__(self):
         return self.name
