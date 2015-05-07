5df14cbcf6215cd1e24ac8cb3f3246ab4384d601
Annie Daniel <adaniel@texastribune.org>
2015-05-07 17:42:12 -0500

    models for county

--- scuole/counties/models.py
+++ scuole/counties/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class County(models.Model):
+    district = model.CharField(
+        help_text="Districts located withinin county", max_length=200)
+    campus = model.CharField(
+        help_text="Campuses located within county", max_length=200)
+    region = model.CharField(
+        help_text="Region located within county", max_length=100)
