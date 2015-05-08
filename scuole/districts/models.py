a95fe18e60926738828d47c38287d6e4aa97cefd
Ryan Murphy <github@accounts.rdmurphy.org>
2015-05-08 15:33:34 -0500

    Add Region model ForeignKey to District

--- scuole/districts/models.py
+++ scuole/districts/models.py
@@ -3,42 +3,45 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
+from scuole.regions.models import Region
+
 
 @python_2_unicode_compatible
 class District(models.Model):
-    # CCD district data #
-    # NAME
+    # CCD - NAME
     name = models.CharField(
         help_text='District name', max_length=200)
     slug = models.SlugField()
-    # STID
+    # TEA - STID
     tea_id = models.CharField(
         help_text='TEA district identifier',
         max_length=6)
-    # LSTREE
+    # CCD - LSTREE
     street = models.CharField(
         help_text='District street',
         max_length=200)
-    # LCITY
+    # CCD - LCITY
     city = models.CharField(
         help_text='District office city',
         max_length=100)
-    # LSTATE
+    # CCD - LSTATE
     state = models.CharField(
         help_text='District office abbreviated state location',
         max_length=2)
-    # LZIP
+    # CCD - LZIP
     zip_code = models.CharField(
         help_text='District office ZIP Code',
         max_length=5)
-    # LZIP4
+    # CCD - LZIP4
     zip_code4 = models.CharField(
         help_text='District office +4 ZIP Code',
         max_length=4)
-    # LATCOD
+    # CCD - LATCOD
     latitude = models.FloatField(help_text='District office latitude')
-    # LONCOD
+    # CCD - LONCOD
     longitude = models.FloatField(help_text='District office longitude')
+    region = models.ForeignKey(
+        Region, related_name='districts', null=True, blank=True)
 
     def __str__(self):
         return self.name
