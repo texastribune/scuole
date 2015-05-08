21fbb0b36db23d1f367e543ddd781c7e71745d90
Annie Daniel <adaniel@texastribune.org>
2015-05-08 15:03:35 -0500

    cleaned up models for counties

--- scuole/counties/models.py
+++ scuole/counties/models.py
@@ -1,13 +1,18 @@
-from __future__ import unicode_literals\
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
 
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
+from ..states.models import State
 
+
+@python_2_unicode_compatible
 class County(models.Model):
     name = models.CharField(help_text="County name", max_length=100)
     slug = models.SlugField()
-    fips = models.IntegerField(help_text="County fips code")
+    fips = models.CharField(help_text="County fips code", max_length=3)
+    state = models.ForeignKey(State, related_name='counties')
 
     def __str__(self):
         return self.name
