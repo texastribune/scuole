141305c96d8763e23346310c3feb72a18d6b6004
Ryan Murphy <github@accounts.rdmurphy.org>
2015-05-08 11:13:09 -0500

    Simplify State model; add migration

--- scuole/states/models.py
+++ scuole/states/models.py
@@ -1,13 +1,13 @@
-from django.db import models
+from __future__ import unicode_literals
 
-# Create your models here.
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+
+@python_2_unicode_compatible
 class State(models.Model):
-    # This data is current from 2012-2013 CCD unlike
-    # School and District which are both from 2013-2014.
     name = models.CharField(help_text="State name", max_length=50)
-    phone = models.CharField(help_text="State phone number", max_length=10)
-    city = models.CharField(help_text="State city", max_length=200)
-    zip = models.CharField(help_text="State zip", max_length=5)
-    zip4 = models.CharField(help_text="State +4 zip", max_length=4)
-    latitude = models.FloatField(help_text="State latitude")
-    longitude = models.FloatField(help_text="State longitude")
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
