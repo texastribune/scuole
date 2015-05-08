e9954ad3f8fbe51f716dceaf88d6527453f49af3
Ryan Murphy <github@accounts.rdmurphy.org>
2015-05-08 15:34:05 -0500

    Alter District loader to only use districts that show up in TEA dataset

--- scuole/districts/management/commands/bootstrapdistricts.py
+++ scuole/districts/management/commands/bootstrapdistricts.py
@@ -9,16 +9,23 @@ from django.core.management.base import BaseCommand
 from django.utils.text import slugify
 
 from ...models import District
+from scuole.regions.models import Region
 
 
 class Command(BaseCommand):
-    help = 'Bootstraps District models using CCD data.'
+    help = 'Bootstraps District models using TEA and CCD data.'
 
     def handle(self, *args, **options):
-        districts_file = os.path.join(
+        ccd_file_location = os.path.join(
             settings.DATA_FOLDER, 'ccd', 'tx-districts-ccd.csv')
 
-        with open(districts_file, 'rb') as f:
+        self.ccd_data = self.load_ccd_file(ccd_file_location)
+
+        tea_file = os.path.join(
+            settings.DATA_FOLDER,
+            'tapr', '2013-14', 'district', 'district-reference.csv')
+
+        with open(tea_file, 'rb') as f:
             reader = csv.DictReader(f)
 
             districts = []
@@ -28,16 +35,30 @@ class Command(BaseCommand):
 
             District.objects.bulk_create(districts)
 
+    def load_ccd_file(self, file):
+        payload = {}
+
+        with open(file, 'rb') as f:
+            reader = csv.DictReader(f)
+
+            for row in reader:
+                payload[row['STID']] = row
+
+        return payload
+
     def create_district(self, district):
+        ccd_match = self.ccd_data[district['DISTRICT']]
+
         return District(
-            name=district['NAME'],
-            slug=slugify(district['NAME']),
-            tea_id=district['STID'],
-            street=district['LSTREE'],
-            city=district['LCITY'],
-            state=district['LSTATE'],
-            zip_code=district['LZIP'],
-            zip_code4=district['LZIP4'],
-            latitude=district['LATCOD'],
-            longitude=district['LONCOD'],
+            name=ccd_match['NAME'],
+            slug=slugify(ccd_match['NAME']),
+            tea_id=district['DISTRICT'],
+            street=ccd_match['LSTREE'],
+            city=ccd_match['LCITY'],
+            state=ccd_match['LSTATE'],
+            zip_code=ccd_match['LZIP'],
+            zip_code4=ccd_match['LZIP4'],
+            latitude=ccd_match['LATCOD'],
+            longitude=ccd_match['LONCOD'],
+            region=Region.objects.get(region_id=district['REGION']),
         )
