3d0a61fe2cfaa79d7b6ca841f1fb81c0cdc8bf52
Ryan Murphy <github@accounts.rdmurphy.org>
2015-05-12 17:33:14 -0500

    Add replacements file

---
+++ scuole/core/replacements.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+ISD_REPLACEMENT = {
+    'Isd': 'ISD',
+    'Cisd': 'CISD',
+    'Csd': 'CSD',
+    'Ft': 'Fort',
+    'Mt': 'Mount',
+    'Ut ': 'UT ',
+    'Llc': 'LLC',
+    '\'brien': '\'Brien',
+    ' Pub Chtr Sch': ' Public Charter School',
+    'Stepping Stones Charter El': 'Stepping Stones Charter Elementary School',
+    ' Of ': ' of ',
+    ' And ': ' and ',
+    ' For ': ' for ',
+    ' In ': ' in ',
+    ' De ': ' de ',
+    '(Ilt)': '(ILT)',
+}
