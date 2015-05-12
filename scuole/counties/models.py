7b2f2fb6b256d7916b85fb828584c1ac1da81347
Ryan Murphy <github@accounts.rdmurphy.org>
2015-05-12 10:41:40 -0500

    More explicit help text on fips field

--- scuole/counties/models.py
+++ scuole/counties/models.py
@@ -11,7 +11,7 @@ from scuole.states.models import State
 class County(models.Model):
     name = models.CharField(help_text="County name", max_length=100)
     slug = models.SlugField()
-    fips = models.CharField(help_text="County fips code", max_length=3)
+    fips = models.CharField(help_text="County FIPS place code", max_length=3)
     state = models.ForeignKey(State, related_name='counties')
 
     def __str__(self):
