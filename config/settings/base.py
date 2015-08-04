b4927280faf60dd0821c45c8398b2e1c2b44fff3
Ryan Murphy <github@accounts.rdmurphy.org>
2015-08-04 16:42:17 -0500

    Add django.contrib.sites to DJANGO_APPS

--- config/settings/base.py
+++ config/settings/base.py
@@ -58,6 +58,7 @@ DJANGO_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.humanize',
     'django.contrib.sessions',
+    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 )
