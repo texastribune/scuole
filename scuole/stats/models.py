9643fae0652d0858982e486eeabe9bb1b0f475c1
Annie Daniel <adaniel@texastribune.org>
2015-09-08 13:29:51 -0500

    so many student demographic models

--- scuole/stats/models.py
+++ scuole/stats/models.py
@@ -13,7 +13,6 @@ class SchoolYear(models.Model):
         return self.name
 
 
-@python_2_unicode_compatible
 class StatsBase(models.Model):
     """
     An abstract model representing stats commonly tracked across all entities
@@ -32,6 +31,8 @@ class StatsBase(models.Model):
         'Number of students', null=True, blank=True)
     african_american_count = models.IntegerField(
         'Number of African American students', null=True, blank=True)
+    american_indian_count = models.FloatField(
+        'Count of American Indian students', null=True, blank=True)
     asian_count = models.IntegerField(
         'Number of Asian students', null=True, blank=True)
     hispanic_count = models.IntegerField(
@@ -42,12 +43,46 @@ class StatsBase(models.Model):
         'Number of Two or More Races students', null=True, blank=True)
     white_count = models.IntegerField(
         'Number of White students', null=True, blank=True)
+    early_childhood_education_count = models.IntegerField(
+        'Number of early childhood education students', null=True, blank=True)
+    prek_count = models.IntegerField(
+        'Number of pre-k students', null=True, blank=True)
+    kindergarten_count = models.IntegerField(
+        'Number of kindergarten students', null=True, blank=True)
+    first_count = models.IntegerField(
+        'Number of first grade students', null=True, blank=True)
+    second_count = models.IntegerField(
+        'Number of second grade students', null=True, blank=True)
+    third_count = models.IntegerField(
+        'Number of third grade students', null=True, blank=True)
+    fourth_count = models.IntegerField(
+        'Number of fourth grade students', null=True, blank=True)
+    fifth_count = models.IntegerField(
+        'Number of fifth grade students', null=True, blank=True)
+    sixth_count = models.IntegerField(
+        'Number of sixth grade students', null=True, blank=True)
+    seventh_count = models.IntegerField(
+        'Number of seventh grade students', null=True, blank=True)
+    eigth_count = models.IntegerField(
+        'Number of eigth grade students', null=True, blank=True)
+    ninth_count = models.IntegerField(
+        'Number of ninth grade students', null=True, blank=True)
+    tenth_count = models.IntegerField(
+        'Number of tenth grade students', null=True, blank=True)
+    eleventh_count = models.IntegerField(
+        'Number of eleventh grade students', null=True, blank=True)
+    twelfth_count = models.IntegerField(
+        'Number of twelfth grade students', null=True, blank=True)
+    at_risk_count = models.IntegerField(
+        'Number of at risk students', null=True, blank=True)
 
     # Student percents
     african_american_percent = models.FloatField(
         'Percent of African American students', null=True, blank=True)
     asian_percent = models.FloatField(
         'Percent of Asian students', null=True, blank=True)
+    american_indian_percent = models.FloatField(
+        'Percent of American Indian students', null=True, blank=True)
     hispanic_percent = models.FloatField(
         'Percent of Hispanic students', null=True, blank=True)
     pacific_islander_percent = models.FloatField(
@@ -56,6 +91,39 @@ class StatsBase(models.Model):
         'Percent of Two or More Races students', null=True, blank=True)
     white_percent = models.FloatField(
         'Percent of White students', null=True, blank=True)
+    early_childhood_education_percent = models.FloatField(
+        'Percent of early childhood education students', null=True, blank=True)
+    prek_percent = models.FloatField(
+        'Percent of pre-k students', null=True, blank=True)
+    kindergarten_percent = models.FloatField(
+        'Percent of kindergarten students', null=True, blank=True)
+    first_percent = models.FloatField(
+        'Percent of first grade students', null=True, blank=True)
+    second_percent = models.FloatField(
+        'Percent of second grade students', null=True, blank=True)
+    third_percent = models.FloatField(
+        'Percent of third grade students', null=True, blank=True)
+    fourth_percent = models.FloatField(
+        'Percent of fourth grade students', null=True, blank=True)
+    fifth_percent = models.FloatField(
+        'Percent of fifth grade students', null=True, blank=True)
+    sixth_percent = models.FloatField(
+        'Percent of sixth grade students', null=True, blank=True)
+    seventh_percent = models.FloatField(
+        'Percent of seventh grade students', null=True, blank=True)
+    eighth_percent = models.FloatField(
+        'Percent of eighth grade students' null=True, blank=True)
+    ninth_percent = models.FloatField(
+        'Percent of ninth grade students', null=True, blank=True)
+    tenth_percent = models.FloatField(
+        'Percent of tenth grade students', null=True, blank=True)
+    eleventh_percent = models.FloatField(
+        'Percent of eleventh grade students', null=True)
+    twelfth_perceont = models.FloatField(
+        'Percent of twelfth grade students', null=True, blank=False)
+    at_risk = models.FloatField(
+        'Percent of at risk students', null=True, blank=False)
+
 
     class Meta:
         abstract = True
