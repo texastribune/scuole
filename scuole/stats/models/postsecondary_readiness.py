c1524fde2dff8de41f9447e55cd90cc3ba56281d
Ryan Murphy <github@accounts.rdmurphy.org>
2015-09-29 17:26:50 -0500

    Remove unused python2 compatible call

--- scuole/stats/models/postsecondary_readiness.py
+++ scuole/stats/models/postsecondary_readiness.py
@@ -2,10 +2,8 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 
 
-@python_2_unicode_compatible
 class PostSecondaryReadinessBase(models.Model):
     """
     An abstract model representing postsecondary readiness data commonly
