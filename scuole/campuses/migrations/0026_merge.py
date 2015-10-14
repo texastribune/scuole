a41175c5ffa93bb755727b4a5176e30dbe32d5e7
Ryan Murphy <rdmurphy@users.noreply.github.com>
2015-10-14 11:37:08 -0500

    Fix conflicting migrations

---
+++ scuole/campuses/migrations/0026_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('campuses', '0025_auto_20151008_1520'),
+        ('campuses', '0025_principal_fax_number_extension'),
+    ]
+
+    operations = [
+    ]
