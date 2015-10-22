1eb91e18ada5c217bc4dea25d2c4dcb1db5c814b
Ryan Murphy <rdmurphy@users.noreply.github.com>
2015-10-22 15:48:40 -0500

    Add merge migrations

---
+++ scuole/regions/migrations/0021_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('regions', '0020_auto_20151021_1905'),
+        ('regions', '0018_auto_20151014_2053'),
+    ]
+
+    operations = [
+    ]
