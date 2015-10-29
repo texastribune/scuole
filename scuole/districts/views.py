4ac5c6f211c9b274c7631341eb49c6267f6bbcc0
Ryan Murphy <rdmurphy@users.noreply.github.com>
2015-10-29 15:32:36 -0500

    Commit all the things, bad Ryan

--- scuole/districts/views.py
+++ scuole/districts/views.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
+from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView, ListView
 
-from .models import District
+from .models import District, DistrictStats
+from scuole.states.models import StateStats
+from scuole.stats.models import SchoolYear
 
 
 class DistrictListView(ListView):
@@ -12,6 +15,28 @@ class DistrictListView(ListView):
 
 class DistrictDetailView(DetailView):
     queryset = District.objects.all().prefetch_related(
-        'campuses', 'stats__year')
+        'campuses')
+
     pk_url_kwarg = 'district_id'
     slug_url_kwarg = 'district_slug'
+
+    def get_context_data(self, **kwargs):
+        context = super(DistrictDetailView, self).get_context_data(**kwargs)
+
+        year = self.kwargs['district_year']
+
+        if year:
+            context['stat'] = get_object_or_404(
+                DistrictStats, year__name=year, district=self.object)
+            context['state'] = get_object_or_404(
+                StateStats, year__name=year, state__name='TX')
+        else:
+            latest_year = SchoolYear.objects.first()
+
+            context['stat'] = get_object_or_404(
+                DistrictStats, year=latest_year,
+                district=self.object)
+            context['state'] = get_object_or_404(
+                StateStats, year=latest_year, state__name='TX')
+
+        return context
