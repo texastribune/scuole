17ece0e3f3eb75e65e3b31e197c28239e028602a
Ryan Murphy <rdmurphy@users.noreply.github.com>
2015-11-09 16:09:57 -0600

    Add whitenoise

--- config/wsgi.py
+++ config/wsgi.py
@@ -9,6 +9,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from whitenoise.django import DjangoWhiteNoise
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
