9a1d52826e285c9d31c6d55ce782a294cac29ea7
Ryan Murphy <rdmurphy@users.noreply.github.com>
2015-11-09 23:09:46 -0600

    The kick off script

---
+++ docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Run app
+exec gunicorn config.wsgi:application \
+  --workers 3 \
+  --bind 0.0.0.0:8000 \
+  "$@"
