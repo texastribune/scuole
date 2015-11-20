15232a12d799c8eeea19605ceea413992b7562f5
Ryan Murphy <rdmurphy@users.noreply.github.com>
2015-11-20 14:47:51 -0600

    Hacky, but a little better metric management

--- scuole/stats/templatetags/stats_tags.py
+++ scuole/stats/templatetags/stats_tags.py
@@ -24,23 +24,46 @@ def get_by_key(value, key):
 
 
 @register.filter
-def display_value(value, value_type=None):
-    if not value:
-        return 'N/A'
+def get_value_display(value, value_type):
+    if value is None:
+        return ('N/A', '')
 
     if value < 0:
-        return 'Masked'
+        return ('Masked', '')
 
     if value_type == 'dollars':
-        return intcomma(floatformat(value, "0"))
+        return (intcomma(floatformat(value, "0")), '$')
 
-    if value_type in ('percent', 'years'):
-        return floatformat(value)
+    if value_type == 'percent':
+        return (floatformat(value), '%')
+
+    if value_type == 'years':
+        return (floatformat(value), 'years')
 
     if value_type == 'test_score':
-        return value
+        return (value, '')
 
     if value_type == 'number':
-        return intcomma(floatformat(value))
+        return (intcomma(floatformat(value)), '')
 
     return value
+
+
+@register.simple_tag
+def display_stat(stat, key, value_type=None, value_label=False):
+    value = get_by_key(stat, key)
+    display_value, display_descriptor = get_value_display(value, value_type)
+
+    if value_label and display_descriptor != '$':
+        html = '<span class="metric-value-label"> {}</span>'.format(
+            display_descriptor)
+        display_descriptor = html
+
+    if display_descriptor == '$':
+        output = '{}{}'.format(display_descriptor, display_value)
+    elif display_descriptor == 'years':
+        output = '{} {}'.format(display_value, display_descriptor)
+    else:
+        output = '{}{}'.format(display_value, display_descriptor)
+
+    return output
