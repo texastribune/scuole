782d99bbe70b94e079ae14e56516e8b74f4d256c
Ryan Murphy <rdmurphy@users.noreply.github.com>
2015-12-07 13:16:35 -0600

    Grand hookup of sitemaps

---
+++ scuole/campuses/sitemaps.py
@@ -0,0 +1,11 @@
+from django.contrib.sitemaps import Sitemap
+
+from scuole.campuses.models import Campus
+
+
+class CampusSitemap(Sitemap):
+    changefreq = 'yearly'
+    priority = 0.5
+
+    def items(self):
+        return Campus.objects.select_related('district')
