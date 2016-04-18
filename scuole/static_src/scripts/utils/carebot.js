fa4dabbf90ddad6c3865b7225dfa9cff2a1a6d50
Ryan Murphy <rdmurphy@users.noreply.github.com>
2016-04-18 11:54:27 -0500

    Hook up carebot scroll tracking

---
+++ scuole/static_src/scripts/utils/carebot.js
@@ -0,0 +1,15 @@
+/* global ga */
+
+import CarebotTracker from 'carebot-tracker';
+
+var tracker = new CarebotTracker.ScrollTracker('main', (percent, seconds) => {
+  var eventData = {
+    'hitType': 'event',
+    'eventCategory': window.location.pathname,
+    'eventAction': 'scroll-depth',
+    'eventLabel': percent,
+    'eventValue': seconds
+  };
+
+  ga('send', eventData);
+});
