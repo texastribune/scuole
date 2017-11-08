bdcf95f7cb69c29e4e0e3abab491b91905b3d0f0
anniedaniel <adaniel@texastribune.org>
2017-11-08 15:08:27 -0600

    update district askted data

--- scuole/districts/management/commands/updateaskted.py
+++ scuole/districts/management/commands/updateaskted.py
@@ -28,10 +28,10 @@ class Command(BaseCommand):
     help = 'Update AskTED data.'
 
     def handle(self, *args, **options):
-        askted_file_location = os.path.join(
+        askted_csv = os.path.join(
             settings.DATA_FOLDER, 'askted', 'directory.csv')
 
-        self.askted_data = self.load_askted_file(askted_file_location)
+        # self.askted_data = self.load_askted_file(askted_file_location)
 
         superintendent_csv = os.path.join(
             settings.DATA_FOLDER,
@@ -40,111 +40,101 @@ class Command(BaseCommand):
         # self.superintendent_data = self.load_superintendent_file(
         #     superintendent_csv)
 
-        with open(superintendent_csv) as f:
+        with open(askted_csv) as f:
             reader = csv.DictReader(f)
 
             for row in reader:
                 self.update_district(row)
 
-    def load_askted_file(self, file):
-        payload = {}
+    # def load_askted_file(self, file):
+    #     payload = {}
 
-        with open(file, 'rU') as f:
-            reader = csv.DictReader(f)
+    #     with open(file, 'rU') as f:
+    #         reader = csv.DictReader(f)
 
-            for row in reader:
-                tea_id = row['District Number'].replace("'", "")
-                payload[tea_id] = row
+    #         for row in reader:
+    #             tea_id = row['District Number'].replace("'", "")
+    #             payload[tea_id] = row
 
-        return payload
+    #     return payload
 
     def update_district(self, district):
         district_id = str(district['District Number']).replace("'", "")
-        district_name = str(district['District Name'])
+        district_name = district['District Name']
+
         try:
-            district = District.objects.get(tea_id=district_id)
-            district.phone_number = district['District Phone']
-            district.zipcode = district['District ']
+            district_match = District.objects.get(tea_id=district_id)
+
+            phone_number = district['District Phone']
+            fax_number = district['District Fax']
+
+            if 'ext' in phone_number:
+                phone_number, phone_number_extension = phone_number.split(' ext:')
+                phone_number_extension = str(phone_number_extension)
+            else:
+                phone_number_extension = ''
+
+            if 'ext' in fax_number:
+                fax_number, fax_number_extension = fax_number.split(' ext:')
+                fax_number_extension = str(phone_number_extension)
+            else:
+                fax_number_extension = ''
+
+            district_match.phone_number = phone_number
+            district_match.phone_number_extension = phone_number_extension
+            district_match.fax_number = fax_number
+            district_match.fax_number_extension = fax_number_extension
+            district_match.street = district['District Street Address']
+            district_match.city = district['District City']
+            district_match.state = district['District State']
+            district_match.zip_code = district['District Zip']
+            district_match.website = district['District Web Page Address']
+            district_match.save()
+
+            print(district_match.name)
         except ObjectDoesNotExist:
             self.stderr.write('No askted data for {}'.format(district_name))
 
-        print(district_name)
+    # def update_superintendent(self, superintendent):
+    #     district_id = str(superintendent['District Number']).replace("'", "")
+    #     district_name = str(superintendent['District Name'])
+    #     try:
+    #         # district_tea = District.objects.get(tea_id=district_id)
+    #         Superintendent.objects.filter(district__tea_id=district_id).update(name=superintendent['Full Name'])
 
+    #         name = '{} {}'.format(
+    #             superintendent['First Name'], superintendent['Last Name'])
+    #         name = string.capwords(name)
+    #         phone_number = superintendent['Phone']
+    #         fax_number = superintendent['Fax']
 
-
-
-    #     if district['DISTRICT'] in self.askted_data:
-    #         askted_match = self.askted_data[district['DISTRICT']]
-    #         phone_number = askted_match['District Phone']
     #         if 'ext' in phone_number:
-    #             phone_number, phone_number_extension = phone_number.split(
-    #                 ' ext:')
+    #             phone_number, phone_number_extension = phone_number.split(' ext:')
     #             phone_number_extension = str(phone_number_extension)
     #         else:
     #             phone_number_extension = ''
-    #         street = askted_match['District Street Address']
-    #         city = askted_match['District City']
-    #         state = askted_match['District State']
-    #         zip_code = askted_match['District Zip']
-    #         website = askted_match['District Web Page Address']
-    #     else:
-    #         self.stderr.write('No askted data for {}'.format(name))
-    #         phone_number = ''
-    #         phone_number_extension = ''
-    #         street = ''
-    #         city = ''
-    #         state = ''
-    #         zip_code = ''
-    #         website = ''
 
-    #     instance, _ = District.objects.update_or_create(
-    #         tea_id=district['DISTRICT'],
-    #         defaults={
-    #             'phone_number': phone_number,
-    #             'phone_number_extension': phone_number_extension,
-    #             'website': website,
-    #             'street': street,
-    #             'city': city,
-    #             'state': state,
-    #             'zip_code': zip_code,
-    #         }
-    #     )
+    #         if 'ext' in fax_number:
+    #             fax_number, fax_number_extension = fax_number.split(' ext:')
+    #             fax_number_extension = str(phone_number_extension)
+    #         else:
+    #             fax_number_extension = ''
 
-    #     if district['DISTRICT'] in self.superintendent_data:
-    #         superintendent = self.superintendent_data[
-    #             district['DISTRICT']]
-    #         self.load_superintendent(instance, superintendent)
-    #     else:
-    #         self.stderr.write('No superintendent data for {}'.format(name))
+    #         instance, _ = Superintendent.objects.upate_or_create(
+    #             name=name,
+    #             district=district,
+    #             defaults={
+    #                 'role': string.capwords(superintendent['Role']),
+    #                 'email': superintendent['Email Address'],
+    #                 'phone_number': phone_number,
+    #                 'phone_number_extension': phone_number_extension,
+    #                 'fax_number': fax_number,
+    #                 'fax_number_extension': fax_number_extension,
+    #             })
 
-    # def load_superintendent(self, district, superintendent):
-    #     name = '{} {}'.format(
-    #         superintendent['First Name'], superintendent['Last Name'])
-    #     name = string.capwords(name)
-    #     phone_number = superintendent['Phone']
-    #     fax_number = superintendent['Fax']
+    #     except ObjectDoesNotExist:
+    #         self.stderr.write('No askted data for {}'.format(district_name))
 
-    #     if 'ext' in phone_number:
-    #         phone_number, phone_number_extension = phone_number.split(' ext:')
-    #         phone_number_extension = str(phone_number_extension)
-    #     else:
-    #         phone_number_extension = ''
+    #     print(district_name)
 
-    #     if 'ext' in fax_number:
-    #         fax_number, fax_number_extension = fax_number.split(' ext:')
-    #         fax_number_extension = str(phone_number_extension)
-    #     else:
-    #         fax_number_extension = ''
 
-    #     Superintendent.objects.update_or_create(
-    #         name=name,
-    #         district=district,
-    #         defaults={
-    #             'role': string.capwords(superintendent['Role']),
-    #             'email': superintendent['Email Address'],
-    #             'phone_number': phone_number,
-    #             'phone_number_extension': phone_number_extension,
-    #             'fax_number': fax_number,
-    #             'fax_number_extension': fax_number_extension,
-    #         }
-    #     )
