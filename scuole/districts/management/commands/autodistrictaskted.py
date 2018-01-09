2a78cadcc40d617f8a416d04a6a34cb758513ea5
anniedaniel <adaniel@texastribune.org>
2018-01-09 16:40:59 -0600

    loads superintendent and directory files

--- scuole/districts/management/commands/autodistrictaskted.py
+++ scuole/districts/management/commands/autodistrictaskted.py
@@ -5,6 +5,7 @@ import csv
 import os
 import string
 import requests
+import datetime
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
@@ -16,10 +17,49 @@ class Command(BaseCommand):
     help = 'Update AskTED data every day!'
 
     def handle(self, *args, **options):
-        self.load_askted_superintendent_csv()
+        askted_folder = os.path.join(
+            settings.DATA_FOLDER,
+            'askted')
 
-    def load_askted_superintendent_csv(self):
+        district_folder = os.path.join(
+            settings.DATA_FOLDER,
+            'askted', 'district')
+
+        self.load_askted_directory_csv(askted_folder)
+        self.load_superintendent_directory_csv(district_folder)
+
+    def load_askted_directory_csv(self, file):
+        url = 'http://tea4avholly.tea.state.tx.us/TEA.AskTED.Web/Forms/DownloadFile.aspx'
+        payload = {
+            '_VIEWSTATE':'',
+            '__VIEWSTATEGENERATOR':'44F2C40C',
+            'ddlSortOrder':'School+Number',
+            'btnDownloadFile':'Download+File'
+        }
+
+        req = requests.post(url, data=payload)
+        reader = csv.DictReader(req.text.splitlines())
+        date = str(datetime.datetime.now().date())
+        directoryFilename = file + '/' + date + '_askTedDirectory.csv'
+        isDirectoryFile = os.path.isfile(directoryFilename)
+
+        def createNewDistrictFile():
+            with open(directoryFilename, 'w+') as csv_file:
+                writer = csv.writer(csv_file, delimiter=',')
+                print(writer)
+                for row in reader:
+                    writer.writerow(row)
+                    # print(row['District Name'])
+
+        if not isDirectoryFile:
+            createNewDistrictFile()
+        else:
+            print("there's already a file here!")
+
+    def load_superintendent_directory_csv(self, file):
+        # url where general directory lives
         url = 'http://tea4avholly.tea.state.tx.us/TEA.AskTED.Web/Forms/DownloadFile2.aspx'
+        # request params
         data = {
             '__VIEWSTATE': '',
             '__VIEWSTATEGENERATOR': '3B1DF71D',
@@ -32,16 +72,23 @@ class Command(BaseCommand):
 
         req = requests.post(url, data=data)
         reader = csv.DictReader(req.text.splitlines())
+        date = str(datetime.datetime.now().date())
+        superintendentFilename = file + '/' + date + '_askTedSuperindendent.csv'
+        isSuperintendentFile = os.path.isfile(superintendentFilename)
 
-        askted_update = os.path.join(
-            settings.DATA_FOLDER,
-            'askted', 'district')
+        def createNewSuptFile():
+            with open(superintendentFilename, 'w+') as csv_file:
+                writer = csv.writer(csv_file, delimiter=',')
+                print(writer)
+                for row in reader:
+                    writer.writerow(row)
+                    # print(row['District Name'])
 
-        with open(askted_update + '/askted_update.csv', 'w+') as csv_file:
-            writer = csv.writer(csv_file, delimiter=',')
-            for row in reader:
-                writer.writerow(row)
-                # print(row['District Name'])
+        if not isSuperintendentFile:
+            createNewSuptFile()
+        else:
+            print("there's already a file here!")
+
 
         # incorporate loading data into models here
         # no need to push to new file yet, maybe if we want to archive
