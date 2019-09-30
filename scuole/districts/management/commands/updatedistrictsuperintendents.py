7b6e8fbe0c9b6900e166e5248b72ab8351c8efcd
Mandi Cai <mcai@texastribune.org>
2019-09-30 12:16:45 -0500

    Take out debugging statement

--- scuole/districts/management/commands/updatedistrictsuperintendents.py
+++ scuole/districts/management/commands/updatedistrictsuperintendents.py
@@ -45,7 +45,6 @@ class Command(BaseCommand):
 
         try:
             district = District.objects.get(tea_id=district_id)
-            self.stdout.write(f"{district}")
         except District.DoesNotExist:
             # if there's no match don't worry about it
             return
