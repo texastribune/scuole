85a81217ca6a22283712d606279cd33208c67fc6
csessig86 <csessig86@gmail.com>
2020-08-19 14:33:37 -0500

    testing Sentry

--- config/urls.py
+++ config/urls.py
@@ -40,7 +40,11 @@ sitemaps = {
     "scuole.core": StaticSitemap,
 }
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
 urlpatterns = [
+    path('sentry-debug/', trigger_error),
     path("", LandingView.as_view(), name="landing"),
     path("outcomes/", include("scuole.cohorts.urls", namespace="cohorts")),
     path("districts/", include("scuole.districts.urls", namespace="districts")),
