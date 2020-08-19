16147948070808d4e08bba84846f74451f6f3cea
csessig86 <csessig86@gmail.com>
2020-08-19 14:06:03 -0500

    remove sentry error message

--- config/settings/production.py
+++ config/settings/production.py
@@ -145,8 +145,6 @@ from sentry_sdk.integrations.django import DjangoIntegration
 
 sentry_sdk.init(dsn=env("SENTRY_DSN", "None"), integrations=[DjangoIntegration()])
 
-Sentry.captureException(new Error("This is my fake error message"));
-
 ############################
 # MIDDLEWARE CONFIGURATION #
 ############################
