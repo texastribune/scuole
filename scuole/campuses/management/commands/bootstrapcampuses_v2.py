cd377f4b1649df5bc9a23c688f6e1d452a532ff3
reliable rascal <reliablerascal@gmail.com>
2025-02-04 16:05:44 -0600

    syntax update

--- scuole/campuses/management/commands/bootstrapcampuses_v2.py
+++ scuole/campuses/management/commands/bootstrapcampuses_v2.py
@@ -49,7 +49,8 @@ class Command(BaseCommand):
                 if raw_id:
                     raw_id = str(int(raw_id.replace("'", '')))
                 else:
-                    print(f"\033[31mWarning: Missing School_Num for properties: {properties}\033[0m")
+                    #print(f"\033[31mWarning: Missing School_Num for properties: {properties}\033[0m")
+                    print(f"\033[31mWarning: Missing School_Num for USER_District_Name: {properties.get("USER_District_Name")}\033[0m")
                     continue
                 tea_id = raw_id
 
